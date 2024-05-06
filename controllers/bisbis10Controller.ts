c7e19f01176a8a351012a0574b055677be81d20c
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2024-05-06 23:46:47 +0300

    Handle cuisine query in GET /restaurants

--- controllers/bisbis10Controller.ts
+++ controllers/bisbis10Controller.ts
@@ -8,6 +8,16 @@ router.get("/", (req: Request, res: Response) => {
 });
 
 router.get("/restaurants", async (req: Request, res: Response) => {
+  const { cuisine } = req.query;
+
+  if (cuisine) {
+    const result = await client.query(
+      "SELECT * FROM restaurants WHERE $1 = ANY(cuisines);",
+      [cuisine as string]
+    );
+    return res.status(200).send(result.rows);
+  }
+
   const result = await client.query(
     'SELECT id, name, "averageRating", "isKosher", cuisines FROM restaurants;'
   );
