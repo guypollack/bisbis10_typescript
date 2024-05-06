486bf04bddad4096de1a455f34f89a667eec5bd7
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2024-05-06 23:55:30 +0300

    Refactor GET /restaurants with one return statement

--- controllers/bisbis10Controller.ts
+++ controllers/bisbis10Controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import client from "../db/db";
+import { QueryResult } from "pg";
 
 const router = Router();
 
@@ -10,17 +11,19 @@ router.get("/", (req: Request, res: Response) => {
 router.get("/restaurants", async (req: Request, res: Response) => {
   const { cuisine } = req.query;
 
+  let result: QueryResult;
+
   if (cuisine) {
-    const result = await client.query(
-      "SELECT * FROM restaurants WHERE $1 = ANY(cuisines);",
+    result = await client.query(
+      'SELECT id, name, "averageRating", "isKosher", cuisines FROM restaurants WHERE $1 = ANY(cuisines);',
       [cuisine as string]
     );
-    return res.status(200).send(result.rows);
+  } else {
+    result = await client.query(
+      'SELECT id, name, "averageRating", "isKosher", cuisines FROM restaurants;'
+    );
   }
 
-  const result = await client.query(
-    'SELECT id, name, "averageRating", "isKosher", cuisines FROM restaurants;'
-  );
   return res.status(200).send(result.rows);
 });
 
