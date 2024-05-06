e8d033fb7f9f7d5fb0b990895208355a0d4ab356
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2024-05-06 23:45:19 +0300

    Implement GET /restaurants route

--- controllers/bisbis10Controller.ts
+++ controllers/bisbis10Controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response, Router } from "express";
+import client from "../db/db";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Welcome to BISBIS10 Server");
+});
+
+router.get("/restaurants", async (req: Request, res: Response) => {
+  const result = await client.query(
+    'SELECT id, name, "averageRating", "isKosher", cuisines FROM restaurants;'
+  );
+  return res.status(200).send(result.rows);
 });
 
 export default router;
