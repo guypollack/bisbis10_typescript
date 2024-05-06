38c7dd874d69da4bc8ac3e60bf6e44c413b08c9c
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2024-05-07 00:08:01 +0300

    Create types.ts

---
+++ types/types.ts
@@ -0,0 +1,21 @@
+export interface Dish {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
+
+export interface Restaurant {
+  id: number;
+  name: string;
+  averageRating: number;
+  isKosher: boolean;
+  cuisines: string[];
+  dishes: Dish[];
+}
+
+export interface Rating {
+  id: number;
+  restaurantId: number;
+  rating: number;
+}
