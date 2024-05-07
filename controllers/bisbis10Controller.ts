0ec7cdf583aa9d11f97ca54fc26a330dab470f23
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2024-05-07 19:21:19 +0300

    Create and use validateRestaurantIdMiddleware

--- controllers/bisbis10Controller.ts
+++ controllers/bisbis10Controller.ts
@@ -1,9 +1,39 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import client from "../db/db";
 import { QueryConfig, QueryResult } from "pg";
 import { Restaurant } from "../types/types";
 
 const router = Router();
+
+export const validateRestaurantIdMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  if (
+    isNaN(Number.parseInt(id)) ||
+    Number.parseInt(id) !== Number.parseFloat(id)
+  ) {
+    return res.status(400).send("Bad Request. id must be an integer");
+  }
+
+  try {
+    const checkExistenceResponse = await client.query(
+      "SELECT * FROM restaurants WHERE id = $1",
+      [id]
+    );
+
+    if (checkExistenceResponse.rowCount === 0) {
+      return res
+        .status(404)
+        .send("The restaurant with the specified id was not found");
+    }
+  } catch (err) {}
+
+  next();
+};
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Welcome to BISBIS10 Server");
@@ -29,32 +59,23 @@ router.get("/restaurants", async (req: Request, res: Response) => {
   return res.status(200).send(result.rows);
 });
 
-router.get("/restaurants/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+router.get(
+  "/restaurants/:id",
+  [validateRestaurantIdMiddleware],
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  if (
-    isNaN(Number.parseInt(id)) ||
-    Number.parseInt(id) !== Number.parseFloat(id)
-  ) {
-    return res.status(400).send("Bad Request. id must be an integer");
+    try {
+      const query: QueryConfig = {
+        text: "SELECT * FROM restaurants WHERE id = $1",
+        values: [id],
+      };
+      const result: QueryResult<Restaurant> = await client.query(query);
+
+      return res.status(200).send(result.rows);
+    } catch (err) {}
   }
-
-  try {
-    const query: QueryConfig = {
-      text: "SELECT * FROM restaurants WHERE id = $1",
-      values: [id],
-    };
-    const result: QueryResult<Restaurant> = await client.query(query);
-
-    if (result.rowCount === 0) {
-      return res
-        .status(404)
-        .send("The restaurant with the specified id was not found");
-    }
-
-    return res.status(200).send(result.rows);
-  } catch (err) {}
-});
+);
 
 router.post("/restaurants", async (req: Request, res: Response) => {
   const {
@@ -152,152 +173,124 @@ router.post("/restaurants", async (req: Request, res: Response) => {
   return res.status(201).send();
 });
 
-router.put("/restaurants/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const {
-    name,
-    isKosher,
-    cuisines,
-  }: Pick<Restaurant, "name" | "isKosher" | "cuisines"> = req.body;
+router.put(
+  "/restaurants/:id",
+  [validateRestaurantIdMiddleware],
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const {
+      name,
+      isKosher,
+      cuisines,
+    }: Pick<Restaurant, "name" | "isKosher" | "cuisines"> = req.body;
 
-  if (
-    isNaN(Number.parseInt(id)) ||
-    Number.parseInt(id) !== Number.parseFloat(id)
-  ) {
-    return res.status(400).send("Bad Request. id must be an integer");
-  }
+    const allowedProperties = ["name", "isKosher", "cuisines"];
 
-  const allowedProperties = ["name", "isKosher", "cuisines"];
+    // Check for not allowed properties being sent in request body
+    const forbiddenProperties = ["id", "averageRating", "dishes"];
 
-  // Check for not allowed properties being sent in request body
-  const forbiddenProperties = ["id", "averageRating", "dishes"];
-
-  const forbiddenPropertiesInRequest = forbiddenProperties.filter((property) =>
-    Object.keys(req.body).includes(property)
-  );
-
-  if (forbiddenPropertiesInRequest.length > 0) {
-    return res
-      .status(422)
-      .send(
-        `Unprocessable Entity. The following properties cannot be updated: ${forbiddenPropertiesInRequest.join(
-          ", "
-        )}`
-      );
-  }
-
-  // Check for unrecognized properties being sent in request body
-  const unrecognizedPropertiesInRequest = Object.keys(req.body).filter(
-    (property) =>
-      !allowedProperties.includes(property) &&
-      !forbiddenProperties.includes(property)
-  );
-
-  if (unrecognizedPropertiesInRequest.length > 0) {
-    return res
-      .status(400)
-      .send(
-        `Bad Request. Unrecognized properties in request: ${unrecognizedPropertiesInRequest.join(
-          ", "
-        )}`
-      );
-  }
-
-  // Check that all data types of columns to be updated are correct
-  if (name !== undefined && typeof name !== "string") {
-    return res.status(400).send("Bad Request. name must be a string");
-  }
-
-  if (isKosher !== undefined && typeof isKosher !== "boolean") {
-    return res.status(400).send("Bad Request. isKosher must be a boolean");
-  }
-
-  if (
-    cuisines !== undefined &&
-    !(
-      Array.isArray(cuisines) &&
-      cuisines.every((elem) => typeof elem === "string")
-    )
-  ) {
-    return res
-      .status(400)
-      .send("Bad Request. cuisines must be an array of strings");
-  }
-
-  // Compose update query
-  const columnsToUpdate = allowedProperties.filter((property) =>
-    Object.keys(req.body).includes(property)
-  );
-
-  const setClauseComponents: string[] = [];
-  const setClauseParameters: Pick<
-    Restaurant,
-    "name" | "isKosher" | "cuisines"
-  >[] = [];
-
-  columnsToUpdate.forEach((columnName, index) => {
-    setClauseComponents.push(`"${columnName}" = $${index + 2}`);
-    setClauseParameters.push(req.body[columnName]);
-  });
-
-  try {
-    const checkExistenceResponse = await client.query(
-      "SELECT * FROM restaurants WHERE id = $1",
-      [id]
+    const forbiddenPropertiesInRequest = forbiddenProperties.filter(
+      (property) => Object.keys(req.body).includes(property)
     );
 
-    if (checkExistenceResponse.rowCount === 0) {
+    if (forbiddenPropertiesInRequest.length > 0) {
       return res
-        .status(404)
-        .send("The restaurant with the specified id was not found");
+        .status(422)
+        .send(
+          `Unprocessable Entity. The following properties cannot be updated: ${forbiddenPropertiesInRequest.join(
+            ", "
+          )}`
+        );
     }
 
-    const query: QueryConfig = {
-      text: `
+    // Check for unrecognized properties being sent in request body
+    const unrecognizedPropertiesInRequest = Object.keys(req.body).filter(
+      (property) =>
+        !allowedProperties.includes(property) &&
+        !forbiddenProperties.includes(property)
+    );
+
+    if (unrecognizedPropertiesInRequest.length > 0) {
+      return res
+        .status(400)
+        .send(
+          `Bad Request. Unrecognized properties in request: ${unrecognizedPropertiesInRequest.join(
+            ", "
+          )}`
+        );
+    }
+
+    // Check that all data types of columns to be updated are correct
+    if (name !== undefined && typeof name !== "string") {
+      return res.status(400).send("Bad Request. name must be a string");
+    }
+
+    if (isKosher !== undefined && typeof isKosher !== "boolean") {
+      return res.status(400).send("Bad Request. isKosher must be a boolean");
+    }
+
+    if (
+      cuisines !== undefined &&
+      !(
+        Array.isArray(cuisines) &&
+        cuisines.every((elem) => typeof elem === "string")
+      )
+    ) {
+      return res
+        .status(400)
+        .send("Bad Request. cuisines must be an array of strings");
+    }
+
+    // Compose update query
+    const columnsToUpdate = allowedProperties.filter((property) =>
+      Object.keys(req.body).includes(property)
+    );
+
+    const setClauseComponents: string[] = [];
+    const setClauseParameters: Pick<
+      Restaurant,
+      "name" | "isKosher" | "cuisines"
+    >[] = [];
+
+    columnsToUpdate.forEach((columnName, index) => {
+      setClauseComponents.push(`"${columnName}" = $${index + 2}`);
+      setClauseParameters.push(req.body[columnName]);
+    });
+
+    try {
+      const query: QueryConfig = {
+        text: `
         UPDATE restaurants
         SET ${setClauseComponents.join(", ")}
         WHERE id = $1
       ;`,
-      values: [id, ...setClauseParameters],
-    };
+        values: [id, ...setClauseParameters],
+      };
 
-    await client.query(query);
-  } catch (err) {}
+      await client.query(query);
+    } catch (err) {}
 
-  return res.status(200).send();
-});
-
-router.delete("/restaurants/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  if (
-    isNaN(Number.parseInt(id)) ||
-    Number.parseInt(id) !== Number.parseFloat(id)
-  ) {
-    return res.status(400).send("Bad Request. id must be an integer");
+    return res.status(200).send();
   }
+);
 
-  try {
-    const checkExistenceResponse = await client.query(
-      "SELECT * FROM restaurants WHERE id = $1",
-      [id]
-    );
+router.delete(
+  "/restaurants/:id",
+  [validateRestaurantIdMiddleware],
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-    if (checkExistenceResponse.rowCount === 0) {
-      return res
-        .status(404)
-        .send("The restaurant with the specified id was not found");
-    }
+    try {
+      const query: QueryConfig = {
+        text: "DELETE FROM restaurants WHERE id = $1",
+        values: [id],
+      };
 
-    const query: QueryConfig = {
-      text: "DELETE FROM restaurants WHERE id = $1",
-      values: [id],
-    };
+      await client.query(query);
 
-    await client.query(query);
-
-    return res.status(204).send();
-  } catch (err) {}
-});
+      return res.status(204).send();
+    } catch (err) {}
+  }
+);
 
 export default router;
