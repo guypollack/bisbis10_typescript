9075391c1859fe608e01fe922a2bea9611d9eb10
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2024-05-07 13:51:25 +0300

    Update error handling in POST /restaurants

--- controllers/bisbis10Controller.ts
+++ controllers/bisbis10Controller.ts
@@ -58,24 +58,40 @@ router.post("/restaurants", async (req: Request, res: Response) => {
     cuisines,
   }: Pick<Restaurant, "name" | "isKosher" | "cuisines"> = req.body;
 
-  if (name === undefined) {
-    return res.status(400).send("Invalid request: missing property 'name'");
+  const requiredProperties = ["name", "isKosher", "cuisines"];
+
+  const missingProperties = requiredProperties.filter(
+    (property) =>
+      !Object.keys(req.body).includes(property) ||
+      req.body[property] === undefined
+  );
+
+  const forbiddenProperties = ["id", "averageRating", "dishes"].filter(
+    (property) => Object.keys(req.body).includes(property)
+  );
+
+  const unrecognizedProperties = Object.keys(req.body).filter(
+    (property) =>
+      !requiredProperties.includes(property) &&
+      !forbiddenProperties.includes(property)
+  );
+
+  if (missingProperties.length > 0) {
+    return res
+      .status(400)
+      .send(
+        `Bad request. Required properties are missing: ${missingProperties.join(
+          ", "
+        )}`
+      );
   }
 
   if (typeof name !== "string") {
-    return res.status(400).send("Invalid request: name must be a string");
-  }
-
-  if (isKosher === undefined) {
-    return res.status(400).send("Invalid request: missing property 'isKosher'");
+    return res.status(400).send("Bad Request. name must be a string");
   }
 
   if (typeof isKosher !== "boolean") {
-    return res.status(400).send("Invalid request: isKosher must be a boolean");
-  }
-
-  if (cuisines === undefined) {
-    return res.status(400).send("Invalid request: missing property 'cuisines'");
+    return res.status(400).send("Bad Request. isKosher must be a boolean");
   }
 
   if (
@@ -86,7 +102,27 @@ router.post("/restaurants", async (req: Request, res: Response) => {
   ) {
     return res
       .status(400)
-      .send("Invalid request: cuisines must be an array of strings");
+      .send("Bad Request. cuisines must be an array of strings");
+  }
+
+  if (unrecognizedProperties.length > 0) {
+    return res
+      .status(400)
+      .send(
+        `Bad Request. Unrecognized properties in request: ${unrecognizedProperties.join(
+          ", "
+        )}`
+      );
+  }
+
+  if (forbiddenProperties.length > 0) {
+    return res
+      .status(422)
+      .send(
+        `Unprocessable Entity. The following properties cannot be included in the creation request: ${forbiddenProperties.join(
+          ", "
+        )}`
+      );
   }
 
   try {
